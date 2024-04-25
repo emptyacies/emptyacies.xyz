0f3cdbe2ed1b1adc38cb5fcfeee8172b9ce9873f
emptyacies <67151752+emptyacies@users.noreply.github.com>
2024-04-25 04:00:29 +0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,13 @@
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+const position = document.documentElement;
+position.style.setProperty("--x", "0px");
+position.addEventListener("mousemove", e => {
+    position.style.setProperty("--x", e.clientX + "px");
+})
+
+document.querySelectorAll('.randomize').forEach(function (element) {
+    var randomValue = Math.random() * (5 - 0.2) + 0.2;
+    element.style.setProperty('--i', randomValue);
+});
+    
